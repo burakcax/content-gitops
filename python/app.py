4fa451e31af0a4d0c7fda0f0b3bab7ee92b34764
burakcax <76398553+burakcax@users.noreply.github.com>
2022-12-07 12:39:56 +0100

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Burak Cataltepe!"
+    return "Hello from the feature branch!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
